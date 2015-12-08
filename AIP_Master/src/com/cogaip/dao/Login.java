a7c1fb0488fda5110f6849b044bb69a6b6bc1f70
genetron <jigar8192@gmail.com>
2015-12-08 10:55:40 +0530

    Added Comment in LoginDAO Class

--- AIP_Master/src/com/cogaip/dao/Login.java
+++ AIP_Master/src/com/cogaip/dao/Login.java
@@ -3,5 +3,6 @@ package com.cogaip.dao;
 import java.sql.ResultSet;
 
 public interface Login extends DAO {
+	//overridden the parent interface method
 	public ResultSet selectRecord(String userName);
 }
