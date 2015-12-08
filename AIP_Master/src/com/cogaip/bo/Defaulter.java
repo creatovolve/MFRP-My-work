e68fa211f917fbb75417e06729c6b0006be6fa45
Jaydeep-Ranipa <jaydeep.ranipa@gmail.com>
2015-12-08 10:24:57 +0530

    BO package commit. interface Defaulter : created defaulter specific abstract methods. class Defaulter : implemented above methods partially.

--- AIP_Master/src/com/cogaip/bo/Defaulter.java
+++ AIP_Master/src/com/cogaip/bo/Defaulter.java
@@ -1,6 +1,14 @@
 package com.cogaip.bo;
 
+import java.util.List;
+
+import com.cogaip.model.DefaulterTO;
+
 public interface Defaulter {
 	// TODO method prototypes of DefaulterBO class.
+	public List<DefaulterTO> selectAllAutoDefaulters();
+	public List<DefaulterTO> selectAllManualDefaulters();
+	public List<DefaulterTO> selectAllRedefaulters();
 	
+	public int updateDefaultStatusByUserId(int userId);
 }
--- AIP_Master/src/com/cogaip/bo/DefaulterBO.java
+++ AIP_Master/src/com/cogaip/bo/DefaulterBO.java
@@ -1,5 +1,33 @@
 package com.cogaip.bo;
 
+import java.util.List;
+
+import com.cogaip.model.DefaulterTO;
+
 public class DefaulterBO implements Defaulter {
+
+	@Override
+	public List<DefaulterTO> selectAllAutoDefaulters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DefaulterTO> selectAllManualDefaulters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DefaulterTO> selectAllRedefaulters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateDefaultStatusByUserId(int userId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
